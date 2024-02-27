793e023fd674715b05135303c1c6d5a2026c6c9b
s.wyss <stefan.arnold.wyss@gmail.com>
2024-02-27 22:03:52 +0100

    feat (Init): Add new spring-project

---
+++ FusionBazaarBackend/src/main/java/com/fusionbazaar/backend/app/FusionBazaarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fusionbazaar.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionBazaarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FusionBazaarBackendApplication.class, args);
+    }
+
+}
